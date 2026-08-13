69ab3eb371cd590834ab11b2405d319918658072
alpondith <sujonalimran@gmail.com>
2026-08-13 19:19:35 +0200

    feat: add dynamic width and height defaults and badge variant to skeleton component

--- resources/views/components/display/skeleton.blade.php
+++ resources/views/components/display/skeleton.blade.php
@@ -5,13 +5,20 @@
 ])
 
 @php
+    $hasClassWidth = preg_match('/(?:^|\s)w-/', $attributes->get('class', ''));
+    $hasClassHeight = preg_match('/(?:^|\s)h-/', $attributes->get('class', ''));
+
     $baseClasses = 'animate-pulse bg-zinc-200 dark:bg-zinc-800';
+
+    $widthClass = $hasClassWidth ? '' : 'w-full ';
+    $heightClass = $hasClassHeight ? '' : 'h-4 ';
 
     $variantClasses = match ($variant) {
         'avatar', 'circle' => 'w-10 h-10 rounded-full shrink-0',
         'button' => 'w-24 h-10 rounded-xl shrink-0',
-        'card' => 'w-full h-48 rounded-2xl',
-        default => 'w-full h-4 rounded-md',
+        'badge' => 'w-16 h-6 rounded-full shrink-0',
+        'card' => "{$widthClass}h-48 rounded-2xl",
+        default => "{$widthClass}{$heightClass}rounded-md",
     };
 
     $style = array_filter([
